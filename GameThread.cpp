836ff2c7dfd9fde303765bf72858fc5778d40fec
Aaravan Thanienayagam <athanie@uwo.ca>
2024-04-03 15:32:08 +0000

    Create Thread class for game and connect to server

--- GameThread.cpp
+++ GameThread.cpp
@@ -1,5 +1,4 @@
 #include "thread.h"
-#include "socketserver.h"
 #include "socket.h"
 #include <stdlib.h>
 #include <time.h>
@@ -9,9 +8,24 @@
 
 using namespace Sync;
 
+class GameThread : public Thread{
+    private:
+    Socket& sock;
+    bool& flag;
 
+    public:
+        GameThread(Socket& s, bool& f) : sock(s), flag(f){} // get access to socket and flag
+        ~GameThread(){} // destructor to kill thread
+    
+    // manage thread from game
+    virtual long ThreadMain() override{
+        sock.Close(); // close connection
+    }
+};
 
 int main(void)
 {
-    
+    bool flag = true;
+    Socket gameSock("127.0.0.1", 3000); // connect to server
+    GameThread gThread(gameSock, flag);
 }
