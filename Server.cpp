e5f3de8f7d456a97a86af3eb114a4b6f5154576a
Aaravan Thanienayagam <athanie@uwo.ca>
2024-04-03 15:29:54 +0000

    Create Thread class to manage games and connect to them

--- Server.cpp
+++ Server.cpp
@@ -9,8 +9,31 @@
 
 using namespace Sync;
 
+class GameManager : public Thread{
+    private:
+    Socket& sock;
+    bool& flag;
+
+    public:
+        GameManager(Socket& s, bool& f) : sock(s), flag(f){} // get access to socket and flag
+        ~GameManager(){} // destructor to kill thread
+    
+    // manage thread from game
+    virtual long ThreadMain() override{
+        sock.Close(); // close connection
+    }
+};
 
 int main(void)
 {
+    bool gameFlag = true;
+    SocketServer sockServer(3000); // create server socket
+    Socket gameSocket = sockServer.Accept(); // accepts connections to game
+    GameManager gameThread(gameSocket, gameFlag);
+    // run threadMain from GameManager
+    while(true){
+        sleep(1);
+    }
     
+
 }
