fa9c72d5767f540e5faab9fea80fa811521d79c6
Aaravan Thanienayagam <athanie@uwo.ca>
2024-04-03 14:58:39 +0000

    Add necessary files

---
+++ Client.cpp
@@ -0,0 +1,15 @@
+#include "socket.h"
+#include "thread.h"
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <limits>
+
+using namespace Sync;
+
+
+
+int main(void)
+{
+
+}
---
+++ Server.cpp
@@ -0,0 +1,16 @@
+#include "thread.h"
+#include "socketserver.h"
+#include "socket.h"
+#include <stdlib.h>
+#include <time.h>
+#include <list>
+#include <vector>
+#include <iostream>
+
+using namespace Sync;
+
+
+int main(void)
+{
+    
+}
